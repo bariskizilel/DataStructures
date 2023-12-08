78bbafeb4a2c9b4e3ee298787e825cf06fe54306
bariskizilel <kizilelb@gmail.com>
2023-12-09 00:18:54 +0300

    CircularSinglyLinkedList

---
+++ src/Lists/CircularSinglyLinkedList.java
@@ -0,0 +1,100 @@
+package Lists;
+
+public class CircularSinglyLinkedList {
+
+    private static Node head;
+
+    public static class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void add(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            newNode.next = head;
+        } else {
+            Node iterator = head;
+            while (iterator.next != head) {
+                iterator = iterator.next;
+            }
+            iterator.next = newNode;
+            newNode.next = head;
+        }
+        System.out.println("Element " + value + " is added");
+    }
+
+    public static void remove(int data) {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node iterator = head;
+
+        if (head.value == data) {
+            // Special case: removing head
+            if (head.next == head) {
+                // Only one element
+                head = null;
+            } else {
+                // More than one element
+                while (iterator.next != head) {
+                    iterator = iterator.next;
+                }
+                head = head.next;
+                iterator.next = head;
+            }
+            System.out.println("Element " + data + " is removed");
+            return;
+        }
+
+        do {
+            Node nextNode = iterator.next;
+
+            if (nextNode.value == data) {
+                iterator.next = nextNode.next;
+                System.out.println("Element " + data + " is removed");
+                return;
+            }
+
+            iterator = nextNode;
+        } while (iterator != head);
+
+
+        System.out.println("Element " + data + " is not found");
+    }
+    public static void print(){
+        Node iterator = head;
+
+        while (iterator.next != head) {
+            System.out.print(iterator.value + " ");
+            iterator = iterator.next;
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        CircularSinglyLinkedList.add(5);
+        CircularSinglyLinkedList.add(2);
+        CircularSinglyLinkedList.add(3);
+        CircularSinglyLinkedList.add(7);
+        CircularSinglyLinkedList.add(6);
+
+        CircularSinglyLinkedList.print();
+
+        CircularSinglyLinkedList.remove(3);
+
+        CircularSinglyLinkedList.print();
+
+
+
+    }
+}
+
